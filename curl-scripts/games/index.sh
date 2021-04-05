732d02affd31b41a949a4a1ee6fc8a9211cd56ae
Amalia <amaliaroye@gmail.com>
2021-04-05 15:52:44 -0400

    Move curl scripts into seperate directories

--- curl-scripts/games/index.sh
+++ curl-scripts/games/index.sh
@@ -4,11 +4,6 @@ curl "https://tic-tac-toe-api-development.herokuapp.com/games" \
   --include \
   --request GET \
   --header "Content-Type: application/json" \
-  --data '{
-    "credentials": {
-      "email": "'"${EMAIL}"'",
-      "password": "'"${PASSWORD}"'"
-    }
-  }'
+  --header "Authorization: Bearer ${TOKEN}" \
 
 echo
